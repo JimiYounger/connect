4a176bd1857e0336790513dbe430ea25bb630bb9
JimiYounger <james.younger@purelightpower.com>
2025-02-20 11:30:07 -0600

    removed mock files

--- src/__mocks__/lucide-react.tsx
+++
@@ -1,2 +0,0 @@
-export const ExternalLink = () => <span aria-label="External link">External Link Icon</span>
-export const Maximize2 = () => <span aria-label="Maximize">Maximize Icon</span> 
--- src/__mocks__/next/image.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const NextImage = ({ src, alt, ...props }: any) => {
-  return <img src={src} alt={alt} {...props} />
-}
-
-export default NextImage 
