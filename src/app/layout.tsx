e531d615b5876409016eb523c32a4b78455f8aa2
JimiYounger <james.younger@purelightpower.com>
2025-02-21 19:22:15 -0600

    added manifest and updated for apple logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 // src/app/layout.tsx
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
 import { AuthProvider } from "@/features/auth/context/auth-context";
 import { Toaster } from "@/components/ui/toaster";
 import { getServerSession } from "@/features/auth/utils/auth-server";
@@ -29,18 +29,18 @@ export const metadata: Metadata = {
         width: 1000,
         height: 1000,
         alt: "Connect Thumbnail",
-      },
+      }
     ],
-    type: "website",
+    type: "website"
   },
   twitter: {
     card: "summary_large_image",
     title: "Connect by Purelight",
     description: "Everything You Need In One Place",
     images: [
-      "https://ucarecdn.com/43907444-0ddb-4271-a3ec-c28ab182b972/-/preview/1000x1000/",
-    ],
-  },
+      "https://ucarecdn.com/43907444-0ddb-4271-a3ec-c28ab182b972/-/preview/1000x1000/"
+    ]
+  }
 };
 
 export default async function RootLayout({
@@ -52,6 +52,20 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        {/* iOS: Custom Home Screen Icon & App Title */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="Connect by Purelight" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+
+        {/* Android: Link to Manifest & Set Theme Color */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
